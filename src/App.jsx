6ce8adcb41bbe7ab40b86c00495506623dc3d05a
Latif <latifakan1999@gmail.com>
2025-06-01 13:28:39 +0200

    updated App file and added tailwindcss to vite.config.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './Card.jsx'
+import Nav from './Nav.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <nav class="bg-amber w-screen top-2">
+        <Nav/>
+      </nav>
+      <section>
+        <Card headline="Übernehme die Kontrolle über deine Schulden" text="Du möchtest endlich die Kontrolle über deine Schulden haben? Lass mir die helfen wie du es schaffst deine Schulden bestmöglich abzuzahlen"/>
+        <Card headline="Du bist überfordert?" text="Viele sind überfordert und wissen nicht wie sie die Schulden abzahlen sollen"/>
+        <Card headline="Der richtige Umgang mit Schulden" text="Du hast nicht gelernt wie du mit Schulden umgehen sollst? Keine Sorge ich helfe dir dabei"/>
+        <Card headline="Verschaffe dir eine Übersicht" text="Du bist überfordert und hast keine Übersicht mehr über die Schulden? Lass uns zusammen ein Plan erstellen, so dass du schnell und geplant die Schulden los wirst"/>
+      </section>
+    </div>
   )
 }
 
